586135f822c12ff4ec650b99ff3d6a859b930b47
Gianluca.Polcino <gianluca.polcino.gp@gmail.com>
2022-02-04 17:12:20 +0100

    models correlations done

--- app/Post.php
+++ app/Post.php
@@ -9,6 +9,11 @@ class Post extends Model
 {
     protected $fillable = ['title', 'content', 'slug'];
 
+    public function category(){
+
+        return $this->belongsTo('App\Category');
+    }
+
     public static function generateSlug($title){
 
         $slug = Str::slug($title);
